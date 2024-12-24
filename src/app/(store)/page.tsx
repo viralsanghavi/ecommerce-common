ee3e4c5521d1f935b276fa8fba7d5dd24cfdedbd
viralsanghavi <vsanghavi3@gmail.com>
2024-12-24 11:58:05 +0530

    Initialize the UI

---
+++ src/app/(store)/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Hello World!</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
---
+++ src/app/(store)/search/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+async function SearchPage({searchParams}: {searchParams: {query: string}}) {
+  const {query} = await searchParams;
+  return <div>SearchPage for {query} </div>;
+}
+
+export default SearchPage;
